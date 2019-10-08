0bb20332af20f3c0e1602d944cf134e641c0731a
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-10-08 12:07:46 +0200

    Added real functionality

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+import os
 
+root_path = "/home/ges/Documents/src"
+repository_dirs = []
+
+def find_all_repositories():
+    for root, dirs, files in os.walk(root_path, followlinks=True):
+        for d in dirs:
+            subdir = os.path.join(root, d)
+            if os.path.isdir(os.path.join(subdir, ".git/")):
+                repository_dirs.append(subdir)
 
 class DemoExtension(Extension):
 
     def __init__(self):
         super(DemoExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-
+        
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
+        repos = []
+        for repo in repository_dirs:
+            repos.append(ExtensionResultItem(icon='images/icon.png',
+                                             name='%s' % i,
+                                             description='Item description %s' % os.path.basename(repo),
                                              on_enter=HideWindowAction()))
 
-        return RenderResultListAction(items)
+        return RenderResultListAction(repos)
 
 if __name__ == '__main__':
     DemoExtension().run()
